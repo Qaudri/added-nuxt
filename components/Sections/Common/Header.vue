80a75aea5ec51dd13d62f0ddfd40b56b637849fe
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 14:17:57 +0100

    chore: header component build in progress

---
+++ components/Sections/Common/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="relative w-full z-50">
+    <div class="shadow-lg fixed bg-white top-0 w-full container mx-auto py-3 px-10">
+      <div class="block lg:flex items-center justify-between">
+
+        <div class="flex justify-between">
+          <UiSharedLogo  />
+
+          <div class="lg:hidden menu-wrap">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
