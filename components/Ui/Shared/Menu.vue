b028f642fc071b43120744c9be806076a928bbd8
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 15:02:24 +0100

    chore: menu and close button components linked on menu component

---
+++ components/Ui/Shared/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-between">
+    <div>
+      <UiButtonsMenu /> 
+      <UiButtonsClose />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
