d219d6ae51cd51ee27216de3e3fa13c303253f0f
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 14:11:44 +0100

    chore: secondary button componet build completed

---
+++ components/Ui/Buttons/Secondary.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button class="text-black text-base lg:mx-4 lg:my-0 xl:text-xl font-medium hover:border-primary-100 hover:text-primary-100 
+    border-b-2 border-transparent ease-in-out block my-4 mx-10 md:lg-0 duration-500">
+      {{button_title}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+props: {
+  button_title: String,
+}
+}
+</script>
+
+<style>
+
+</style>
