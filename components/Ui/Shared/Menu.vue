e850459160fdb48d3e5fd59befe7d55a6fafc7b4
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 15:08:14 +0100

    chore: menu items created on menu component

--- components/Ui/Shared/Menu.vue
+++ components/Ui/Shared/Menu.vue
@@ -4,6 +4,14 @@
       <UiButtonsMenu /> 
       <UiButtonsClose />
     </div>
+
+    <div class="items-center hidden lg:flex lg:pt-0 pt-8 h-screen lg:h-auto">
+      <UiButtonsSecondary button_title="Who We Are" />
+      <UiButtonsSecondary button_title="What We Do" />
+      <UiButtonsSecondary button_title="Our Services" />
+      <UiButtonsSecondary button_title="Academy" />
+      <UiButtonsPrimary button_title="Let's Talk" />
+    </div>
   </div>
 </template>
 
