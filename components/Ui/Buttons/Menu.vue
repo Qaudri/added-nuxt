bc4d308e88053d6d2dd5e65a1bfd6b0c989be56c
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 14:53:06 +0100

    chore: menu button component build completed

---
+++ components/Ui/Buttons/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="require('@/assets/images/icons/menu.svg')" class="block w-10" alt="" @click="revealMenuEvent">
+</template>
+
+<script>
+export default {
+  methods: {
+    revealMenuEvent(){
+      this.$emit("revealMenu")
+    }
+  }
+}
+</script>
