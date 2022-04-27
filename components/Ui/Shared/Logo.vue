4a5b437b4cd105573c92f6081da67e050e687531
Qaudri <aquadirmuhammad@gmail.com>
2022-04-27 13:57:41 +0100

    chore: logo component build completed

---
+++ components/Ui/Shared/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img :src="require('@/assets/images/icons/branding/main-logo.svg')" alt="" class="block w-36 2xl:w-40" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
