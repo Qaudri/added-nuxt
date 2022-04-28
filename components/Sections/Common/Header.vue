f8fd4d3244a6a20829319a15909b8ef45878693d
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 14:49:40 +0100

    refactor: secondary button component margin

--- components/Sections/Common/Header.vue
+++ components/Sections/Common/Header.vue
@@ -9,10 +9,10 @@
         </div>
 
         <div class="items-center hidden lg:flex lg:pt-0 pt-8 h-screen lg:h-auto">
-          <UiButtonsSecondary button_title="Who We Are" />
-          <UiButtonsSecondary button_title="What We Do" />
-          <UiButtonsSecondary button_title="Our Services" />
-          <UiButtonsSecondary button_title="Academy" />
+          <UiButtonsSecondary button_title="Who We Are" class="lg:mx-4 lg:my-0 my-4 ml-10 md:ml-0"/>
+          <UiButtonsSecondary button_title="What We Do" class="lg:mx-4 lg:my-0 my-4 ml-10 md:ml-0"/>
+          <UiButtonsSecondary button_title="Our Services" class="lg:mx-4 lg:my-0 my-4 ml-10 md:ml-0"/>
+          <UiButtonsSecondary button_title="Academy" class="lg:mx-4 lg:my-0 my-4 ml-10 md:ml-0" />
           <UiButtonsPrimary button_title="Let's Talk" class="lg:ml-4 my-10 lg:my-0 mx-6 lg:mr-0 " />
         </div>
 
