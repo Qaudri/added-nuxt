bc91c2d6818f8e3acfd1e5387ab40c072cb0db4f
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 16:23:41 +0100

    chore: footer images linking added

--- components/Sections/Common/Footer.vue
+++ components/Sections/Common/Footer.vue
@@ -4,19 +4,19 @@
       <div class="block lg:flex justify-around w-full items-center">
         <div class=" flex justify-center mx-auto md:mx-0 mb-4 md:mb-0">
           <a target="blank" href="https://www.facebook.com">
-            <img :src="require('/assets/images/facebook.svg')" alt="" width="20px" class="mr-6">
+            <img :src="require('/assets/images/icons/socials/facebook.svg')" alt="" width="20px" class="mr-6">
           </a>
 
           <a target="blank" href="https://www.instagram.com" >
-            <img :src="require('/assets/images/instagram.svg')" alt="" width="20px" class="mr-6">
+            <img :src="require('/assets/images/icons/socials/instagram.svg')" alt="" width="20px" class="mr-6">
           </a>
 
           <a target="blank" href="https://wa.me/2340866699820">
-            <img :src="require('/assets/images/whatsapp.svg')" alt="" width="20px" class="mr-6">
+            <img :src="require('/assets/images/icons/socials/whatsapp.svg')" alt="" width="20px" class="mr-6">
           </a>
 
           <a target="blank" href="https://www.youtube.com" >
-            <img :src="require('/assets/images/youtube.svg')" alt="" width="20px">
+            <img :src="require('/assets/images/icons/socials/youtube.svg')" alt="" width="20px">
           </a>
         </div>
 
