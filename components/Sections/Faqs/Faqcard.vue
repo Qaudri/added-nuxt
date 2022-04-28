a3be87055c59c46df0adcfb4cc733b559751cd74
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 18:38:05 +0100

    chore: faq card design created

---
+++ components/Sections/Faqs/Faqcard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div >
+    <SectionsFaqsQuestion @questionClicked="toggleAnswer" >
+      {{question}}
+    </SectionsFaqsQuestion>
+
+    <SectionsFaqsAnswer :class="answerRevealed ? 'block' : 'hidden'">
+      {{answer}}
+    </SectionsFaqsAnswer>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+
+  props:{
+    question: String,
+    answer: String
+  },
+
+
+  data(){
+    return{
+      answerRevealed: false,
+    }
+  },
+
+  methods: {
+    toggleAnswer(){
+      this.answerRevealed = !this.answerRevealed
+    }
+  },
+
+  // computed: {
+  //   ...mapGetters({
+  //     faqItems : 'faqs/getFaqItems'
+  //   })
+  // }
+
+}
+</script>
+
+<style>
+
+</style>
