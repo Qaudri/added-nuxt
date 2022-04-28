7e884684238bea43d72a27a30da4d27c4c0e36b5
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 18:32:27 +0100

    chore: faq question component created

---
+++ components/Sections/Faqs/Question.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div @click="dropDown" :class="is_clicked ? 'border-b-0 rounded-b-none' : ''"
+  class="w-screen-1/2 flex rounded-lg justify-between px-4 py-3  border-2 border-accent1-600 bg-white
+   text-primary-100 text-lg font-medium">
+    <slot class="cursor-pointer" />
+
+    <img :src="require('@/assets/images/icons/reveal.svg')" alt="" width="10px"
+    :class="is_clicked ? 'hidden' : 'block' " class="">
+
+    <img :src="require('@/assets/images/icons/hide.svg')" alt="" width="10px" 
+    :class="is_clicked ? 'block' : 'hidden' " class="">
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      is_clicked: false,
+    }
+  },
+
+  methods: {
+    dropDown(){
+      this.is_clicked = !this.is_clicked,
+      this.$emit("questionClicked")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
