963a38f5b340618b8339589d764f5ab5635cfb4f
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 17:04:25 +0100

    chore: logo component emit event added

--- components/Ui/Shared/Logo.vue
+++ components/Ui/Shared/Logo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <img :src="require('@/assets/images/icons/branding/main-logo.svg')" alt="" class="block w-36 2xl:w-40" />
+    <img :src="require('@/assets/images/icons/branding/main-logo.svg')" @click="emitReload"
+     alt="" class="cursor-pointer block w-36 2xl:w-40" />
   </div>
 </template>
 
 <script>
 export default {
-
+methods: {
+  emitReload(){
+    this.$emit("emitReload")
+  }
+}
 }
 </script>
 
