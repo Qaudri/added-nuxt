ef87fdef2039e41784fb24157721b049adc51812
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 16:20:18 +0100

    chore: footer code imported

---
+++ components/Sections/Common/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="container mx-auto py-6 px-10">
+      <div class="block lg:flex justify-around w-full items-center">
+        <div class=" flex justify-center mx-auto md:mx-0 mb-4 md:mb-0">
+          <a target="blank" href="https://www.facebook.com">
+            <img :src="require('/assets/images/facebook.svg')" alt="" width="20px" class="mr-6">
+          </a>
+
+          <a target="blank" href="https://www.instagram.com" >
+            <img :src="require('/assets/images/instagram.svg')" alt="" width="20px" class="mr-6">
+          </a>
+
+          <a target="blank" href="https://wa.me/2340866699820">
+            <img :src="require('/assets/images/whatsapp.svg')" alt="" width="20px" class="mr-6">
+          </a>
+
+          <a target="blank" href="https://www.youtube.com" >
+            <img :src="require('/assets/images/youtube.svg')" alt="" width="20px">
+          </a>
+        </div>
+
+        <div class="flex justify-center items-cente lg:blockr mt-2">
+          <div class="text-white font medium opacity-50" >2020 Added Digital Solutions. All rights reserved</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
