a0f9e0e585bcabeaf0101452552b47112173d196
Qaudri <aquadirmuhammad@gmail.com>
2022-04-28 16:38:15 +0100

    chore:social icons width changed

--- components/Sections/Common/Footer.vue
+++ components/Sections/Common/Footer.vue
@@ -4,24 +4,24 @@
       <div class="block lg:flex justify-around w-full items-center">
         <div class=" flex justify-center mx-auto md:mx-0 mb-4 md:mb-0">
           <a target="blank" href="https://www.facebook.com">
-            <img :src="require('/assets/images/icons/socials/facebook.svg')" alt=""  class="mr-6 w-5 2xl:w-8">
+            <img :src="require('/assets/images/icons/socials/facebook.svg')" alt=""  class="mr-6 w-5 2xl:w-7">
           </a>
 
           <a target="blank" href="https://www.instagram.com" >
-            <img :src="require('/assets/images/icons/socials/instagram.svg')" alt=""  class="mr-6 w-5 2xl:w-8">
+            <img :src="require('/assets/images/icons/socials/instagram.svg')" alt=""  class="mr-6 w-5 2xl:w-7">
           </a>
 
-          <a target="blank" href="https://wa.me/2340866699820">
-            <img :src="require('/assets/images/icons/socials/whatsapp.svg')" alt=""  class="mr-6 w-5 2xl:w-8">
+          <a target="blank" href="https://wa.me/2340766699720">
+            <img :src="require('/assets/images/icons/socials/whatsapp.svg')" alt=""  class="mr-6 w-5 2xl:w-7">
           </a>
 
           <a target="blank" href="https://www.youtube.com" >
-            <img :src="require('/assets/images/icons/socials/youtube.svg')" alt="" class=" w-5 2xl:w-8" >
+            <img :src="require('/assets/images/icons/socials/youtube.svg')" alt="" class=" w-5 2xl:w-7" >
           </a>
         </div>
 
         <div class="flex justify-center items-center lg:block mt-2">
-          <div class="text-white font medium opacity-50" >2020 Added Digital Solutions. All rights reserved</div>
+          <div class="text-white xl:text-xl font medium opacity-50" >2020 Added Digital Solutions. All rights reserved</div>
         </div>
       </div>
     </div>
