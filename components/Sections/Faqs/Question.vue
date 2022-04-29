317c3ffc5d3ebb6243ebca1bce770d3f69891ab6
Qaudri <aquadirmuhammad@gmail.com>
2022-04-29 21:46:12 +0100

    refactor: component widths in progress

--- components/Sections/Faqs/Question.vue
+++ components/Sections/Faqs/Question.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div @click="dropDown" :class="is_clicked ? 'border-b-0 rounded-b-none' : ''"
+  <div @click="dropDown" :class="is_clicked ? 'border-b-0' : ''"
   class="w-full md:w-2/3 flex justify-between px-4 py-3 border-2 border-primary-100 bg-white
    text-primary-100 text-lg font-medium mx-auto">
 
