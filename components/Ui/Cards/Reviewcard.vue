f2fb93237bfadee7edb8ae342f9693e9ae522d79
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 17:19:05 +0100

    chore: reviewcard component design in progress

---
+++ components/Ui/Cards/Reviewcard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-gray-50 p-6 width">
+    <div class="text-9xl font-light opacity-20">"</div>
+    <SectionsReviewsReview>
+      {{review}}
+    </SectionsReviewsReview>
+
+    <SectionsReviewsReview>
+      {{reviewer}}
+    </SectionsReviewsReview>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.width{
+  width: 24rem !important;
+  padding-right: 1.5rem !important;
+  height: 26rem !important;
+}
+</style>
