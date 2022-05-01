a089cc15edf64551ee8e7692c966d4583a5a0560
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 16:39:05 +0100

    chore: review vuex file imported on container component

---
+++ components/Sections/Reviews/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+
+  computed: {
+    ...mapGetters({
+      faqItems : 'faqs/getFaqItems'
+    })
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
