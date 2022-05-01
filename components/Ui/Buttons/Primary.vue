5a82d802e48bbac02227e1a60fd8850f41ab9261
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 14:15:27 +0100

    chore: primary button component border responsiveness completed

--- components/Ui/Buttons/Primary.vue
+++ components/Ui/Buttons/Primary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="text-primary-100 xl:text-xl xl:border-4 text-base px-4 py-2 font-medium border-2 border-primary-100 hover:bg-primary-100 hover:text-white duration-500 ease-in-out">
+    <button class="text-primary-100 xl:text-xl xl:border-4 text-base px-4 py-2 font-medium border-2 2xl:border-3 border-primary-100 hover:bg-primary-100 hover:text-white duration-500 ease-in-out">
     {{button_title}}
     </button>
   </div>
