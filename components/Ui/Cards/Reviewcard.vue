e4bd20d99bea75f5e8e71c123d2e95ed4d7f3774
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 17:23:17 +0100

    chore: reviewcard components spacing done

--- components/Ui/Cards/Reviewcard.vue
+++ components/Ui/Cards/Reviewcard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bg-gray-50 p-6 width">
     <div class="text-9xl font-light opacity-20">"</div>
-    <SectionsReviewsReview>
+    <SectionsReviewsReview class="-mt-16 pl-10">
       {{review}}
     </SectionsReviewsReview>
 
-    <SectionsReviewsReview>
+    <SectionsReviewsReviewer class="mt-5 flex justify-end">
       {{reviewer}}
-    </SectionsReviewsReview>
+    </SectionsReviewsReviewer>
   </div>
 </template>
 
