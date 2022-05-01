2ad5def4323445220189aa7305fbd4fc23639d74
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 16:21:01 +0100

    reviews state items outline created

--- store/reviews.js
+++ store/reviews.js
@@ -1,7 +1,41 @@
 export const state = () => ({
-  item: {
+  item: [
+    {
+      id: 1,
+      review: "",
+      reviewer: "",
+    },
 
-  }
+    {
+      id: 2,
+      review: "",
+      reviewer: "",
+    },
+
+    {
+      id: 3,
+      review: "",
+      reviewer: "",
+    },
+
+    {
+      id: 4,
+      review: "",
+      reviewer: "",
+    },
+
+    {
+      id: 5,
+      review: "",
+      reviewer: "",
+    },
+
+    {
+      id: 6,
+      review: "",
+      reviewer: "",
+    },
+  ]
 })
 
 export const getters = {
