b0c03090a741118001085bbef71c887172286fef
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 16:41:46 +0100

    chore: review component design complete

---
+++ components/Sections/Reviews/Review.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot class="cursor-pointer text-base md:text-lg xl:text-xl text-secondary-100" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
