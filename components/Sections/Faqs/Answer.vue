659e2ff290c1da7cc9c71ce9f3502fb65a02c234
Qaudri <aquadirmuhammad@gmail.com>
2022-05-01 15:56:57 +0100

    refactor: question and answer components border and padding

--- components/Sections/Faqs/Answer.vue
+++ components/Sections/Faqs/Answer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slideDown border-2 2xl:border-3 border-primary-100 border-t-0 w-full
-  mx-auto bg-white shadow-lg px-4 pb-3 pt-2 text-secondary-100 font-normal text-base">
+  <div class="slideDown border-2 2xl:border-3 border-primary-100 2xl:border-t-0 border-t-0 w-full
+  mx-auto bg-white shadow-lg px-4 pb-3 pt-3 pr-6 md:pr-10 text-secondary-100 font-normal text-base">
     <slot class="cursor-pointer" />
   </div>
 </template>
