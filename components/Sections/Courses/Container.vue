73a519c503ac26b3e3fabf06e8bd6729a726d0bd
Qaudri <aquadirmuhammad@gmail.com>
2022-05-04 17:15:33 +0100

    chore: data binding completed

--- components/Sections/Courses/Container.vue
+++ components/Sections/Courses/Container.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container mx-auto px-10 py-28 xl:py-32">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      <UiCardsCoursecard v-for="item in course" :key="item.id" />
+      <UiCardsCoursecard v-for="item in course" :key="item.id" 
+      :imgUrl="item.imageUrl" 
+      :title="item.title" 
+      :duration="item.duration"
+      :price="item.price" />
     </div>
   </div>
 </template>
@@ -12,7 +16,7 @@ import {mapGetters} from 'vuex';
 export default {
   computed: {
     ...mapGetters({
-      course : 'faqs/getCourseItems'
+      course : 'courses/getCourseItems'
     })
   }
 }
