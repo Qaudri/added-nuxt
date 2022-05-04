d03e83a15b806de4b7bba9b3a056738169266632
Qaudri <aquadirmuhammad@gmail.com>
2022-05-04 16:58:43 +0100

    chore: course card component imported on container component

--- components/Sections/Courses/Container.vue
+++ components/Sections/Courses/Container.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto px-10 py-28 xl:py-32">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      
+      <UiCardsCoursecard />
     </div>
   </div>
 </template>
