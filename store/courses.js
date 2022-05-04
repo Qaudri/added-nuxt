0fa1b91108918f598a4c8611dfaa362ba64252d2
Qaudri <aquadirmuhammad@gmail.com>
2022-05-04 15:44:34 +0100

    chore: courses store db outline created

---
+++ store/courses.js
@@ -0,0 +1,84 @@
+export const state = () =>({
+  items: [
+    {
+      id: 1,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 2,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 3,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 4,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 5,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 6,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 7,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 8,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+
+    {
+      id: 9,
+      imageUrl: "",
+      title: "",
+      duration: "",
+      price: "",
+      details: "",
+    },
+  ]
+})
