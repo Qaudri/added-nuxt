9cff1c9ec73da4c808c96b08a8a7f84ffdb07838
Qaudri <aquadirmuhammad@gmail.com>
2022-05-04 14:06:53 +0100

    chore: props types defined

--- components/Sections/Courses/Price.vue
+++ components/Sections/Courses/Price.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <p class="text-base">
-      {{duration}} | <span class="text-primary-100 font-medium">{{price}}</span>
+      {{duration}} | <span class="text-primary-100 font-medium">#{{price}}</span>
     </p>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    duration: String,
+    price: String,
+  }
 }
 </script>
 
