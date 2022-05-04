a062a9f465499252a98931e344d5dbc8f18fb3da
Qaudri <aquadirmuhammad@gmail.com>
2022-05-04 16:54:46 +0100

    chore: container component design in progress

--- components/Sections/Courses/Container.vue
+++ components/Sections/Courses/Container.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <div class="container mx-auto px-10 py-28 xl:py-32">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      
+    </div>
+  </div>
 </template>
 
 <script>
