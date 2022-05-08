d3453aa52c148e47c9debfce9d613618878ca6cf
Qaudri <aquadirmuhammad@gmail.com>
2022-05-08 13:01:55 +0100

    chore: mapgetters imported on new faq component

---
+++ components/Sections/Faqs/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <UiCardsFaqcard />
+  </div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex';
+export default {
+
+  computed: {
+
+      ...mapGetters({
+        reviews : 'reviews/getReviews'
+      })
+  },
+
+
+}
+</script>
+
+<style>
+
+</style>
