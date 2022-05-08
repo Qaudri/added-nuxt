bf0038c23e5b4b6ffb92c877584cfa7ca6813144
Qaudri <aquadirmuhammad@gmail.com>
2022-05-08 13:17:26 +0100

    chore: title component content added on card component

--- components/Ui/Cards/Coursecard.vue
+++ components/Ui/Cards/Coursecard.vue
@@ -7,6 +7,9 @@
     </SectionsCoursesImage>
     <div class="pt-4">
       <SectionsCoursesTitle>
+        <h1 class="text-2xl font-bold">
+          {{title}}
+        </h1>
       </SectionsCoursesTitle>
 
 
