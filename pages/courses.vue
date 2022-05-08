baee1ae2b967052c24e2b432b99ff3272377caee
Qaudri <aquadirmuhammad@gmail.com>
2022-05-08 14:31:43 +0100

    chore: form items styling added

--- pages/courses.vue
+++ pages/courses.vue
@@ -11,7 +11,7 @@
             <label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder="Enter your email address">
 
-            <button>Register</button>
+            <button type="submit">Register</button>
           </form>
         </div>
       </div>
@@ -36,5 +36,50 @@ export default {
 </script>
 
 <style>
+form{
+  width: 100%;
+  display: block;
+  box-sizing: border-box;
+}
 
+label{
+  font-weight: medium;
+  color: #058182;
+  margin-top: 30px;
+  display: block;
+}
+
+input{
+  padding: 10px 15px;
+  margin: 10px 0;
+  display: block;
+  width: 100%;
+  border: 2px solid #222222;
+}
+
+
+input:focus{
+  border: 2px solid #058182;
+  border-radius: 0;
+  outline: none;
+  color: #058182;
+}
+
+.submit{
+  margin-top: 3rem;
+  width: 10rem;
+  background-color: transparent;
+  border: 2px solid #058182;
+  text-transform: capitalize;
+  color: #058182;
+  font-weight: 600;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.submit:hover{
+  background: #058182;
+  color: white;
+  transition: .5s ease-in-out;
+}
 </style>
