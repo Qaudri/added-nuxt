a95d53af2972ae5917ea4b804837dc795a1e9fa2
Qaudri <aquadirmuhammad@gmail.com>
2022-05-08 14:30:20 +0100

    chore: form build complete

--- pages/courses.vue
+++ pages/courses.vue
@@ -4,7 +4,15 @@
     <SectionsCoursesContainer @RegisterUser="register">
       <div class="h-full w-full absolute z-10 bg-black">
         <div class="bg-white p-8">
+          <form action="">
+            <label for="Name">Name</label>
+            <input type="text" name="Name" id="Name" placeholder="Enter your full name">
 
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter your email address">
+
+            <button>Register</button>
+          </form>
         </div>
       </div>
     </SectionsCoursesContainer>
