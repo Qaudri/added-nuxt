6c70475773c8904ad4907bbd16162363273746bf
Qaudri <aquadirmuhammad@gmail.com>
2022-05-10 14:41:54 +0100

    chore: form register function updated

--- pages/courses.vue
+++ pages/courses.vue
@@ -2,7 +2,9 @@
   <div>
     <div :class="formdisplayed ? 'block' : 'hidden'" class="h-full w-full fixed z-10 bg-black bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
       <div class="bg-white p-8 w-full md:w-2/3 lg:w-1/3">
-        <div class="flex items-end"></div>
+        <div class="flex items-end">
+          <UiButtonsClose @closeMenu="toggleForm" />
+        </div>
         <form action="">
           <label for="Name" class="mt-0">Name</label>
           <input type="text" name="Name" id="Name" placeholder="Enter your full name">
@@ -39,8 +41,8 @@ export default {
   },
 
   methods: {
-    register(){
-      this.formdisplayed = true
+    toggleForm(){
+      this.formdisplayed = !this.formdisplayed
     }
   },
 
