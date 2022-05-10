4c2b5ce9a741bd9517186cd7df9bc52574fae1bd
Qaudri <aquadirmuhammad@gmail.com>
2022-05-10 17:30:06 +0100

    Chore: primary button emit function defined

--- components/Ui/Buttons/Primary.vue
+++ components/Ui/Buttons/Primary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="text-primary-100 xl:text-xl text-base px-4 py-2 font-medium border-2 2xl:border-3 border-primary-100 hover:bg-primary-100 hover:text-white duration-500 ease-in-out">
+    <button @click="emitPushToPage" class="text-primary-100 xl:text-xl text-base px-4 py-2 font-medium border-2 2xl:border-3 border-primary-100 hover:bg-primary-100 hover:text-white duration-500 ease-in-out">
     {{button_title}}
     </button>
   </div>
@@ -10,6 +10,12 @@
 export default {
 props: {
   button_title: String,
+},
+
+methods: {
+  emitPushToPage(){
+    this.$emit("pushTo")
+  }
 }
 }
 </script>
