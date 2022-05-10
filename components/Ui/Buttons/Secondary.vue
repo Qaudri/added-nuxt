359dc15951fed25e148d11264b5c7feafa6f49b0
Qaudri <aquadirmuhammad@gmail.com>
2022-05-10 17:01:32 +0100

    chore: emit event defined on secondary button component

--- components/Ui/Buttons/Secondary.vue
+++ components/Ui/Buttons/Secondary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="text-black text-base xl:text-xl font-medium hover:border-primary-100 hover:text-primary-100 
+    <button @click="emitPushtoLink" class="text-black text-base xl:text-xl font-medium hover:border-primary-100 hover:text-primary-100 
     border-b-2 border-transparent ease-in-out block duration-500">
       {{button_title}}
     </button>
@@ -11,6 +11,12 @@
 export default {
 props: {
   button_title: String,
+},
+
+methods: {
+  emitPushtoLink(){
+    this.$emit("Pushto")
+  }
 }
 }
 </script>
