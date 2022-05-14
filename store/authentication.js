b81e9c4f257ec0be68d8764b0d5d4b001c5da265
Qaudri <aquadirmuhammad@gmail.com>
2022-05-14 13:56:17 +0100

    chore: state updated

--- store/authentication.js
+++ store/authentication.js
@@ -1,8 +1,13 @@
 export const state = () => ({
   session_token: process.browser ? localStorage.getItem('ubit_user_session_token') : null || null,
   auth_status: false,
-  username: Noarex,
-  password: Olayemi0102,
+  loginInfo: [
+    {
+    username: "Noarex",
+    password: "Olayemi0102",
+    }
+  ]
+
 })
 
 export const getters = {
