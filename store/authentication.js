4469b712350113e8a53a11d348b331a7a3b0c011
Qaudri <aquadirmuhammad@gmail.com>
2022-05-14 13:42:09 +0100

    chore: state data items upated

--- store/authentication.js
+++ store/authentication.js
@@ -1,4 +1,6 @@
 export const state = () => ({
   session_token: process.browser ? localStorage.getItem('ubit_user_session_token') : null || null,
   auth_status: false,
+  username: Noarex,
+  password: Olayemi0102,
 })
