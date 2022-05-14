98161a5d8e5a7c3ba19dca73c7c078b644bd76ba
Qaudri <aquadirmuhammad@gmail.com>
2022-05-14 14:24:23 +0100

    chore: form build in progress

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="formdisplayed ? 'block' : 'hidden'" class="h-full w-full fixed z-10 bg-black bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
+  <div :class="formdisplayed ? 'block' : 'hidden'" class="h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
     <div class="bg-white p-8 w-full md:w-2/3 lg:w-1/3">
       <form action="">
-        <label for="Name" class="mt-0">Name</label>
-        <input type="text" name="Name" id="Name" placeholder="Enter your full name">
-
-        <label for="email">Email</label>
+        <label for="Name" class="mt-0">Email</label>
         <input type="email" name="email" id="email" placeholder="Enter your email address">
+
+        <label for="email">Password</label>
+        <input type="password" name="password" id="password" placeholder="Enter your password">
 
         <button type="submit" class="submit">Register</button>
       </form>
