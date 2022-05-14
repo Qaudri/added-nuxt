aaaec48a30c26c3ac41eb7b3fa9dbdfc6ca95861
Qaudri <aquadirmuhammad@gmail.com>
2022-05-14 14:29:32 +0100

    chore: submit form action changed

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
     <div class="bg-white p-8 w-full md:w-2/3 lg:w-1/3">
-      <form action="">
+      <form action="" @submit.prevent="attemptLogin">
         <label for="Name" class="mt-0">Email</label>
         <input type="email" name="email" id="email" placeholder="Enter your email address">
 
