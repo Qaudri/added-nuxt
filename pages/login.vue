5e5314516405067a513f8e1ea735d32373c58930
Qaudri <aquadirmuhammad@gmail.com>
2022-05-14 14:25:58 +0100

    chore: data binding removed

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="formdisplayed ? 'block' : 'hidden'" class="h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
+  <div class="h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
     <div class="bg-white p-8 w-full md:w-2/3 lg:w-1/3">
       <form action="">
         <label for="Name" class="mt-0">Email</label>
