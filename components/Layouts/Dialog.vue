1bb376da8c17b39685e1b301b6a2470f48b65fb2
Qaudri <aquadirmuhammad@gmail.com>
2022-05-18 13:57:18 +0100

    chore: dialog component update in progress

--- components/Layouts/Dialog.vue
+++ components/Layouts/Dialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute bg-secondary-100 flex items-center justify-center h-full w-full left-0">
+  <div class="absolute bg-secondary-100 bg-opacity-20 flex items-center justify-center h-full w-full left-0">
 
     <div class="bg-white p-6 pb-0">
 
