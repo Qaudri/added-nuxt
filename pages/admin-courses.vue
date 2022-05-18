949ec061a9c7f83c64020d9de8a1f8b80b764743
Qaudri <aquadirmuhammad@gmail.com>
2022-05-18 12:37:54 +0100

    chore: build updated

--- pages/admin-courses.vue
+++ pages/admin-courses.vue
@@ -5,7 +5,10 @@
 
     </template>
     
-    <div>
+    <div class="p-8 py-14">
+      <div class="py-3 border-b mb-8">
+        <h1 class="text-2xl font-semibold">Courses</h1>
+      </div>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         <UiCardsCoursecard v-for="item in course" :key="item.id" 
         :imgUrl="item.imageUrl" 
