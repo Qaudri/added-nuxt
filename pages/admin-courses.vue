b5b8b152e06f9b0fab52d051a275cd73614bef36
Qaudri <aquadirmuhammad@gmail.com>
2022-05-18 12:33:54 +0100

    chore: coursecard declared in new slot

--- pages/admin-courses.vue
+++ pages/admin-courses.vue
@@ -4,7 +4,16 @@
     <template v-slot:hero class="first-letter:" >
 
     </template>
-
+    
+    <div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <UiCardsCoursecard v-for="item in course" :key="item.id" 
+        :imgUrl="item.imageUrl" 
+        :title="item.title" 
+        :duration="item.duration"
+        :price="item.price" @RegisterUser="toggleForm"/>
+      </div>
+    </div>
   </LayoutsAdminApp>
 </template>
 
