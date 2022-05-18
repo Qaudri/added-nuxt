02b148a48aa2a684c15a6f56740aa0f52debf8b3
Qaudri <aquadirmuhammad@gmail.com>
2022-05-18 12:15:42 +0100

    chore: v-slot used on admin-course page

--- pages/admin-courses.vue
+++ pages/admin-courses.vue
@@ -1,12 +1,14 @@
 <template>
   <LayoutsAdminApp>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      <UiCardsCoursecard v-for="item in course" :key="item.id" 
-      :imgUrl="item.imageUrl" 
-      :title="item.title" 
-      :duration="item.duration"
-      :price="item.price" @RegisterUser="toggleForm"/>
-    </div>
+    <template v-slot:hero >
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <UiCardsCoursecard v-for="item in course" :key="item.id" 
+        :imgUrl="item.imageUrl" 
+        :title="item.title" 
+        :duration="item.duration"
+        :price="item.price" @RegisterUser="toggleForm"/>
+      </div>
+    </template>
   </LayoutsAdminApp>
 </template>
 
@@ -14,6 +16,11 @@
 import {mapGetters} from 'vuex';
 
 export default {
+  methods: {
+    toggleForm(){
+      console.log("Form toggled")
+    }
+  },
   computed: {
     ...mapGetters({
       course : 'courses/getCourseItems'
