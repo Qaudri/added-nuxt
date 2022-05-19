6f7ae23c94fe6af5f317b206e20f1abf248a43ed
Qaudri <aquadirmuhammad@gmail.com>
2022-05-19 16:04:48 +0100

    chore: form build in progress'

--- components/Layouts/Form.vue
+++ components/Layouts/Form.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="absolute bg-secondary-100 z-110 bg-opacity-20 flex items-center justify-center h-full w-full top-0 left-0">
     <div class="bg-white p-6 pb-0">
+      <div>
+        <div class="border-b text-xl font-semibold flex justify-between">
+          <p class="font-medium text-black text-lg">Update Course</p>
+
+          <div class="">
+
+            <button>
+              <slot />
+            </button>
+          </div>
+        </div>
+      </div>
       <form action="" >
         <label for="title">Course title</label>
         <input type="text" :placeholder="course_title">
