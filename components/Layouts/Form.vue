ff4099d50750095e643b03826ee6ea6b5ad4cacd
Qaudri <aquadirmuhammad@gmail.com>
2022-05-19 15:59:14 +0100

    refactor:  form styling in progress

--- components/Layouts/Form.vue
+++ components/Layouts/Form.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="absolute bg-secondary-100 z-110 bg-opacity-20 flex items-center justify-center h-full w-full top-0 left-0">
     <form action="">
       <label for="title">Course title</label>
       <input type="text" placeholder="{{course_title}}">
@@ -27,7 +27,6 @@ props: {
   course_price: String,
   course_description: String,
   course_contents: String,
-
 }
 }
 </script>
