28de0b1bfcec3e42a43cd1eba39c98e7407c5b34
Qaudri <aquadirmuhammad@gmail.com>
2022-05-19 16:06:00 +0100

    refactor: default style profiles added to admin-courses

--- pages/admin-courses.vue
+++ pages/admin-courses.vue
@@ -69,5 +69,13 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap');
 
+body{
+  font-family: "Poppins", sans-serif;
+}
+
+.bg-gray-50{
+  background-color: #f7f7f7 !important;
+}
 </style>
