afc74265a5587173c741452c355b2b3b0bde7399
Qaudri <aquadirmuhammad@gmail.com>
2022-05-19 16:01:07 +0100

    chore: v-bind implemented on form

--- components/Layouts/Form.vue
+++ components/Layouts/Form.vue
@@ -2,19 +2,19 @@
   <div class="absolute bg-secondary-100 z-110 bg-opacity-20 flex items-center justify-center h-full w-full top-0 left-0">
     <form action="">
       <label for="title">Course title</label>
-      <input type="text" placeholder="{{course_title}}">
+      <input type="text" :placeholder="course_title">
 
       <label for="title">Course duration</label>
-      <input type="text" placeholder="{{course_duration}}">
+      <input type="text" :placeholder="course_duration">
 
       <label for="title">Course price</label>
-      <input type="text" placeholder="{{course_price}}">
+      <input type="text" :placeholder="course_price">
 
       <label for="title">Course decription</label>
-      <input type="text" placeholder="{{course_description}}">
+      <input type="text" :placeholder="course_description">
 
       <label for="title">Course contents</label>
-      <input type="text" placeholder="{{course_contents}}">
+      <input type="text" ::placeholder="course_contents">
     </form>
   </div>
 </template>
