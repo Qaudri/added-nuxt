d5fb93120f2c3eef5fba6ce24a2dfe5320f11f1c
Qaudri <aquadirmuhammad@gmail.com>
2022-05-25 13:24:11 +0100

    chore: mapActions declared on courses page

--- pages/courses.vue
+++ pages/courses.vue
@@ -52,7 +52,7 @@
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import {mapGetters, mapActions} from 'vuex';
 
 export default {
   data(){
@@ -77,6 +77,10 @@ export default {
         }
       })
     },
+
+    ...mapActions({
+      setCourse: 'courses/setSelectedCourse'
+    }),
 
     toggleForm(){
       this.formdisplayed = !this.formdisplayed
