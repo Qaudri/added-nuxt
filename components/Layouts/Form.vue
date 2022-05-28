d84d58ef81bf24aa66ab988823eea5ca59159015
Qaudri <aquadirmuhammad@gmail.com>
2022-05-28 14:23:59 +0100

    chore: scale out animation declared on course edit form

--- components/Layouts/Form.vue
+++ components/Layouts/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-10 absolute bg-secondary-100 z-110 bg-opacity-20 flex items-center justify-center h-full w-full top-10 left-0">
-    <div class="bg-white p-6 w-full md:w-2/3 lg:w-1/2">
+    <div class="scale-out bg-white p-6 w-full md:w-2/3 lg:w-1/2">
       <div>
         <div class="border-b text-xl font-semibold flex justify-between">
           <p class="font-medium text-black text-lg">Update Course</p>
