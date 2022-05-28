32d813d0af3d093ba058147a5f2cd11c55c95fe6
Qaudri <aquadirmuhammad@gmail.com>
2022-05-28 13:50:59 +0100

    chore: build in progress

--- pages/courses.vue
+++ pages/courses.vue
@@ -30,8 +30,11 @@
       </template>
     </SectionsCommonHeader>
 
-    <div :class="detailsdisplayed ? 'block' : 'hidden'" class="bg-white relative right-0 h-full w-full px-10 py-28 xl:py-32">
-      <div class="fixed container mx-auto">
+    <div :class="detailsdisplayed ? 'block' : 'hidden'" class="bg-white fixed h-full w-full px-10 py-28 xl:py-32">
+      <div class="container mx-auto">
+        <div class="mb-4 w-full flex justify-end">
+          <UiButtonsClose class="cursor-pointer m-0" />
+        </div>
         <div class="flex items-center ">
           <div class="pr-4 border-r w-32 h-32">
             <img ::src="selected.imageUrl" alt="" class="w-24">
@@ -48,7 +51,10 @@
         <div class="flex">
           <div class="text-lg font-medium mr-12">Course duration: <span class="text-primary-100">{{selected.duration}}</span></div>
           <div class="text-lg font-medium">Course price: <span class="text-primary-100">{{selected.price}}</span></div>
-
+        </div>
+        <div class="flex items-center">
+          <UiButtonsPrimary @pushTo="toggleForm" class="px-4 py-2 text-white font-medium" button_title="Enroll now"/>
+          <UiButtonsSecondary @Pushto="showDetails(item)" button_title="Check Other Courses" class="mx-6"/>
         </div>
       </div>
     </div>
