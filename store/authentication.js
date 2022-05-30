c0421fd9c91c59c2012a970b378e9c97546db0b0
Qaudri <aquadirmuhammad@gmail.com>
2022-05-30 14:22:43 +0100

    chore: user state item created

--- store/authentication.js
+++ store/authentication.js
@@ -1,7 +1,7 @@
 export const state = () => ({
   session_token: process.browser ? localStorage.getItem('ubit_user_session_token') : null || null,
   auth_status: false,
-
+  user: {},
 })
 
 export const getters = {
