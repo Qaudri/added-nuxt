fe59e26788504a610b5a268972205315739b2f77
Qaudri <aquadirmuhammad@gmail.com>
2022-05-30 14:34:56 +0100

    chore: localStorage item session name changed

--- store/authentication.js
+++ store/authentication.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-  session_token: process.browser ? localStorage.getItem('ubit_user_session_token') : null || null,
+  session_token: process.browser ? localStorage.getItem('session_token') : null || null,
   auth_status: false,
   user: {},
 })
