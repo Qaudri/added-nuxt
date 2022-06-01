29fab7c66aa3a2fdc5da01a65a3ba0fbd6f87cbc
Qaudri <aquadirmuhammad@gmail.com>
2022-06-01 13:34:08 +0100

    chore: redundant mapactions deleted

--- pages/login.vue
+++ pages/login.vue
@@ -36,8 +36,7 @@ export default {
   methods: {
 
     ...mapActions({
-      tryLogin : 'authentication/tryLogin',
-      retrieveLoggedUserInfo: 'authentication/retrieveLoggedUserInfo'
+
 
     }),
 
