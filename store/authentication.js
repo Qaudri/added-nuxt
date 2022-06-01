5cd31acc7b5258faf74e4e1508249fbfddb405b6
Qaudri <aquadirmuhammad@gmail.com>
2022-06-01 14:52:49 +0100

    chore: admin actions api urls updated

--- store/authentication.js
+++ store/authentication.js
@@ -60,7 +60,7 @@ export const actions = {
 
   loginAdmin(context, credentials){
     return new Promise((resolve, reject) => {
-      this.$axios.$post('/api/admin/auth/login', {
+      this.$axios.$post('/api/admins/auth/login', {
         email: credentials.email,
         password: credentials.password
       })
@@ -102,7 +102,7 @@ export const actions = {
     return new Promise((resolve, reject) => {
       this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.session_token
 
-      this.$axios.$get('/api/admin/account')
+      this.$axios.$get('/api/admins/account')
         .then(response => {
           context.commit('SET_USER_INFO', response.data)
 
