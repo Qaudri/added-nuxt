9cf782784ba00c886acaa051c54ce073bf9697a1
Qaudri <aquadirmuhammad@gmail.com>
2022-06-01 14:28:27 +0100

    chore: admin login action created

--- store/authentication.js
+++ store/authentication.js
@@ -58,6 +58,29 @@ export const actions = {
     })
   },
 
+  loginAdmin(context, credentials){
+    return new Promise((resolve, reject) => {
+      this.$axios.$post('/api/admin/auth/login', {
+        email: credentials.email,
+        password: credentials.password
+      })
+        .then(response => {
+          context.commit('SET_SESSION_TOKEN', response.data.token)
+          context.commit('SET_AUTHENTICATION_STATUS', true)
+          localStorage.setItem('session_token', response.data.token)
+
+          context.dispatch('retrieveAdminInfo')
+
+          resolve(response)
+        })
+
+
+        .catch(function (error) {
+          reject(error)
+        })
+    })
+  },
+
   retrieveStudentInfo(context, credentials) {
     return new Promise((resolve, reject) => {
       this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.session_token
