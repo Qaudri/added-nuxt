3e4316161627317a0d7bc7f15cde8e524b38e3f2
Qaudri <aquadirmuhammad@gmail.com>
2022-06-08 14:43:30 +0100

    chore: deletecourse action created

--- store/courses.js
+++ store/courses.js
@@ -140,6 +140,23 @@ export const actions = {
     })
   },
 
+  deleteCourse(context, credentials){
+    return new Promise((resolve, reject) => {
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.session_token
+
+      this.$axios.$post('/api/admins/courses/:uuid')
+        .then(response => {
+          context.commit('CREATE_NEW_COURSE', response.data)
+
+          resolve(response)
+        })
+
+        .catch(function (error) {
+          reject(error)
+        })
+    })
+  },
+
 }
 
 export const mutations = {
