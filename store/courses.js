c34450330c9a275581e17ce7bb0fca3a06f3c5f8
Qaudri <aquadirmuhammad@gmail.com>
2022-06-13 14:16:00 +0100

    refactor: deletCourse function updated

--- store/courses.js
+++ store/courses.js
@@ -182,7 +182,12 @@ export const actions = {
     return new Promise((resolve, reject) => {
       this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.rootState.authentication.session_token
 
-      this.$axios.$delete('/api/admins/courses/:uuid')
+      this.$axios.$delete('/api/admins/courses/:uuid',{
+        title: credentials.title,
+        price: credentials.price,
+        brief: credentials.description,
+        preview: credentials.imageUrl,
+      })
         .then(response => {
           resolve(response)
         })
