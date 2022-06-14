f4acfba702b1d12a5860cbc4e7c03bc279cb2258
Qaudri <aquadirmuhammad@gmail.com>
2022-06-14 14:15:42 +0100

    refactor: new style updates issued

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
+  <div class="scale-out h-full w-full fixed z-10 bg-gray-400 bg-opacity-40 flex items-center justify-center backdrop-filter backdrop-blur">
     <div class="bg-white p-8 w-full md:w-2/3 lg:w-1/3">
 
       <form action="" @submit.prevent="signIn">
@@ -119,4 +119,18 @@ export default {
     color: white;
     transition: .5s ease-in-out;
   }
+
+.scale-out{
+  animation: scaleout 1s forwards;
+}
+
+@keyframes scaleout {
+  from{
+    transform: scale(0);
+  }
+
+  to{
+    transform: scale(1);
+  }
+}
 </style>
