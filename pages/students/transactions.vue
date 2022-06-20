d17e604cbd02ccbf39efc8768159d5be44eea6c9
Qaudri <aquadirmuhammad@gmail.com>
2022-06-20 16:01:46 +0100

    chore: transactions actions declared

---
+++ pages/students/transactions.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <LayoutsAdminApp>
+      <template v-slot:hero class="">
+      </template>
+
+    </LayoutsAdminApp>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex';
+
+export default {
+  middleware: 'student-login',
+
+  data(){
+    return{
+    }
+  },
+
+  methods: {
+    ...mapActions({
+      setAllTransactions: 'transactions/setAdminTransactions'
+    }),
+  },
+
+  computed: {
+    ...mapGetters({
+      allTransactions: 'transactions/getStudentTransactions',
+    }),
+  },
+
+  created() {
+    this.showAllCourses()
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap');
+
+body{
+  font-family: "Poppins", sans-serif;
+}
+
+.bg-gray-50{
+  background-color: #f7f7f7 !important;
+}
+</style>
