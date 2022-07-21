0e74be4572d6b30e625f16e6c4e3d3eb13875d11
Qaudri <aquadirmuhammad@gmail.com>
2022-07-21 14:15:11 +0100

    refactor: admin navigation menu button updated

--- components/Ui/Shared/AdminNavigation.vue
+++ components/Ui/Shared/AdminNavigation.vue
@@ -20,8 +20,8 @@
                   
                   <div class="md:hidden flex justify-end" @click="ToggleMobileNav">
                     <div class="w-8">
-                      <div :class="(mobile_nav ? 'transition transform rotate-45 my-0 w-8' : 'transition my-1 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')" class="block cursor-pointer" style="height: 3px;"></div>
-                      <div :class="(mobile_nav ? 'transition transform -rotate-45 my-0 w-8 -mt-0.5' : 'transition my-1 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')"  class="block cursor-pointer" style="height: 3px;"></div>
+                      <div :class="(mobile_nav ? 'transition transform rotate-45 my-0 w-8' : 'transition my-1.5 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')" class="block cursor-pointer" style="height: 3px;"></div>
+                      <div :class="(mobile_nav ? 'transition transform -rotate-45 my-0 w-8 -mt-0.5' : 'transition my-1.5 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')"  class="block cursor-pointer" style="height: 3px;"></div>
                     </div>
                   </div>
                   
