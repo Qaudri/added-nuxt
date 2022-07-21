de4ca4850f63752be2f592a9cd5304f30cf07d84
Qaudri <aquadirmuhammad@gmail.com>
2022-07-21 14:01:34 +0100

    chore: menu button component updated

--- components/Ui/Buttons/Menu.vue
+++ components/Ui/Buttons/Menu.vue
@@ -1,11 +1,26 @@
 <template>
-  <img :src="require('@/assets/images/icons/menu.svg')" class="block w-7" alt="" @click="revealMenuEvent">
+  <div class="md:hidden flex justify-end" @click="revealMenuEvent">
+    <div class="w-8">
+      <div :class="(mobile_nav ? 'transition transform rotate-45 my-0 w-8' : 'transition my-1 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')" class="block cursor-pointer" style="height: 3px;"></div>
+      <div :class="(mobile_nav ? 'transition transform -rotate-45 my-0 w-8 -mt-0.5' : 'transition my-1 w-8') + ' ' + (BlockBackground ? 'bg-primary-100' : 'bg-primary-100')"  class="block cursor-pointer" style="height: 3px;"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    BlockBackground: Boolean
+  },
+
+  data(){
+    return{
+      mobile_nav:false,
+    }
+  },
   methods: {
     revealMenuEvent(){
+      this.mobile_nav = !this.mobile_nav
       this.$emit("revealMenu")
     }
   }
