92663530f1ad6eee2780f8bbf18162e00eb9ad22
Qaudri <aquadirmuhammad@gmail.com>
2022-07-21 13:46:21 +0100

    chore: anchor tags locations added

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -6,8 +6,8 @@
           <div class="bg-white border p-6 justify-between items-center flex">
             <div>
               <p class="font-medium uppercase text-sm">Available courses</p>
-              <h1 class="font-semibold text-4xl mb-3">0</h1>
-              <a href="" class="px-2 py-1 text-sm border">Manage</a>
+              <h1 class="font-semibold text-4xl mb-3">{{courseTotal}}</h1>
+              <a href="/admin/courses" class="px-2 py-1 text-sm border">Manage</a>
             </div>
 
             <img :src="require('@/assets/images/icons/list.svg')" class="p-2 bg-primary-100 w-14" alt="">
@@ -17,7 +17,7 @@
             <div>
               <p class="font-medium uppercase text-sm">Registered Students</p>
               <h1 class="font-semibold text-4xl mb-3">0</h1>
-              <a href="" class="px-2 py-1 text-sm border">Manage</a>
+              <a href="/admin/transactions" class="px-2 py-1 text-sm border">Manage</a>
             </div> 
 
             <img :src="require('@/assets/images/icons/categories.svg')" class="p-2 bg-primary-100 w-14" alt="">
@@ -27,7 +27,7 @@
             <div>
               <p class="font-medium uppercase text-sm">Total Transactions</p>
               <h1 class="font-semibold text-4xl mb-3 max-w-xs">0</h1>
-              <a href="" class="px-2 py-1 text-sm border">Manage</a>
+              <a href="/admin/transactions" class="px-2 py-1 text-sm border">Manage</a>
             </div>
 
             <img :src="require('@/assets/images/icons/earning.svg')" class="p-2 bg-primary-100 w-14" alt="">
