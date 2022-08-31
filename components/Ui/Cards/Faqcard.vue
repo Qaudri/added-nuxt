8ce17a20e56c35fe89a5e573df5fe3a902a86dd2
Qaudri <aquadirmuhammad@gmail.com>
2022-08-31 14:06:30 +0100

    refactor: faqcard component updated

--- components/Ui/Cards/Faqcard.vue
+++ components/Ui/Cards/Faqcard.vue
@@ -3,19 +3,23 @@
     
     <SectionsFaqsAccordion>
 
-      <SectionsFaqsAccordionItem container_class="bg-gray-50 border p-2 my-3">
-
+      <SectionsFaqsAccordionItem container_class="">
         <template #accordion-trigger>
-          <SectionsFaqsQuestion @questionClicked="toggleAnswer" class="w-full" >
-            {{question}}
-          </SectionsFaqsQuestion>
-          </template>
+          <div class="w-full flex justify-between px-4 py-3 border-2 2xl:border-3 border-primary-100 bg-white text-primary-100 text-lg font-medium mx-auto">
+            <h3>{{question}}</h3>
+
+            <img :src="require('@/assets/images/icons/reveal.svg')" alt="" width="10px"
+            :class="is_clicked ? 'hidden' : 'block' " class="ml-3">
+
+            <img :src="require('@/assets/images/icons/hide.svg')" alt="" width="10px" 
+            :class="is_clicked ? 'block' : 'hidden' " class="ml-3">
+          </div>
+        </template>
 
         <template #accordion-content>
-          <SectionsFaqsAnswer :class="answerRevealed ? 'block' : 'hidden'" class="w-full">
-            {{answer}}
-          </SectionsFaqsAnswer>
-
+          <div class="border-2 2xl:border-3 border-primary-100 2xl:border-t-0 border-t-0 w-full mx-auto bg-white shadow-lg px-4 pb-3 pt-3 pr-6 md:pr-10 text-secondary-100 font-normal text-base">
+            <h3>{{answer}}</h3>
+          </div>
         </template>
       </SectionsFaqsAccordionItem>
     </SectionsFaqsAccordion>
@@ -41,13 +45,6 @@ export default {
   },
 
   methods: {
-    toggleAnswer(){
-      this.$emit("checkActive")
-      this.is_active = true
-      if (this.is_active = true) {
-        this.answerRevealed = !this.answerRevealed
-      }
-    },
 
   },
 
